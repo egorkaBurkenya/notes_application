0745b02d16046341aedc622dab752e1d9baceb8b
egorkaBurkenya <sleepyegorka@gmail.com>
2021-05-21 00:28:38 +0300

    fix_url

--- app/db/index.js
+++ app/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/api', {
+mongoose.connect('mongodb+srv://Egor:egor1234@notes.8ftic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
